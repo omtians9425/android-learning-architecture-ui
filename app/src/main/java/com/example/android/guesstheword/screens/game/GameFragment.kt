8dcc98e1054b9d73992f93a52c71bc8a421a186d
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-30 15:31:55 +0900

    declare viewmodel at GameFragment

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,6 +31,8 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -52,6 +55,11 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
+
+        /**
+         * Pre-existing ViewModel is used when configuration change occurs and its Fragment is recreated.
+         */
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
