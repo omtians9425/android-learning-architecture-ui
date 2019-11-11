fbb157db4bdacd35d73407a01e6c12f0c5ea13cb
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-11-11 17:12:24 +0900

    implement to use custom factory at fragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,10 +1,30 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
+
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
+
+    private var _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+    get() = _eventPlayAgain
+
     init {
         Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    //tear down so that [ScoreFragment] does not receive true when recreated.
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
